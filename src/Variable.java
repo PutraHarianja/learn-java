ec175977e341553a0fff1487956edfff028bde02
putra.harianja <putra.harianja007@gmail.com>
2025-02-12 21:03:16 +0700

    learn: variable

---
+++ src/Variable.java
@@ -0,0 +1,23 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "Putra Harianja Gabe";
+
+        int age = 25;
+        String address = "Indonesia";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+
+        name = "Jacob Sitorus";
+        System.out.println(name);
+
+        var firstName = "Putra";
+        var middleName = "Gabe";
+        var lastName = "Harianja";
+
+
+        final String application = "Belajar Java";
+    }
+}
